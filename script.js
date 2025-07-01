a5f6491ab54a43fcc4d966352afef5bdc9dfe14b
Ahmad Basit <ahmadbasit0808@gmail.com>
2025-07-01 12:35:57 +0500

    Added new pages and enhanced interactivity

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ let input1 = document.getElementById("navinput");
 function remove() {
   input1.value = "";
 }
+
+//cart.html
+const deleteButtons = document.querySelectorAll(".delete-btn");
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const itemRow = e.target.closest(".cart-item");
+    itemRow.remove();
+  });
+});
+
+// Delete selected items when the "Delete Selected Items" button is clicked
+document.getElementById("delete-selected-btn").addEventListener("click", () => {
+  const selectedCheckboxes = document.querySelectorAll(
+    ".cart-item-checkbox:checked"
+  );
+  selectedCheckboxes.forEach((checkbox) => {
+    const itemRow = checkbox.closest(".cart-item");
+    itemRow.remove();
+  });
+});
